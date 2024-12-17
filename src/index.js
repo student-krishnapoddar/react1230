117a6c01fcbd673987cdfda5507bdd42449fb750
Krishna Poddar <krishnapoddar504king@gmail.com>
2024-12-17 14:49:42 +0530

    use tc

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Xys, { About, About2 } from './Myabout';
-import Mycontact from './Mycontact';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Abc, { About, About2 } from "./Myabout";
+import Mycontact from "./Mycontact";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "./style.css";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <h1>sdfhsdjkfhj</h1>
     <div>sdjfhjsdhf sdfsdfsdfds</div>
     <About></About>
-    <Xys></Xys>
+    {/* <Xys></Xys> */}
+    <Abc></Abc>
     <About2></About2>
-    <Mycontact/>
+    <Mycontact />
   </React.StrictMode>
 );
-
